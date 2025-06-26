2896aa93b1748a0b60070187d47a93c78254ea9a
abk8406 <abk8406@gmail.com>
2025-06-26 11:30:12 +0530

    remove production env

--- src/service/env.config.ts
+++ src/service/env.config.ts
@@ -1,12 +1,12 @@
 import { EnvConfig } from './env-config.interface';
 
 export const Config: EnvConfig = {
-    BaseEndpoint: 'http://localhost:5000/',
-    LoginEndpoint: 'http://localhost:5000/api/',
-    LogoutEndpoint: 'http://localhost:5000/',
+    // BaseEndpoint: 'http://localhost:5000/',
+    // LoginEndpoint: 'http://localhost:5000/api/',
+    // LogoutEndpoint: 'http://localhost:5000/',
 
-    // BaseEndpoint: 'https://meetnode-production.up.railway.app/',
-    // LoginEndpoint: 'https://meetnode-production.up.railway.app/api/',
-    // LogoutEndpoint: 'https://meetnode-production.up.railway.app/',
+    BaseEndpoint: 'https://meetnode-production.up.railway.app/',
+    LoginEndpoint: 'https://meetnode-production.up.railway.app/api/',
+    LogoutEndpoint: 'https://meetnode-production.up.railway.app/',
 }
 
